d06851ea7647813449d702420037fc323f06177e
brianspinos777 <brianspinos777@hotmail.com>
2022-12-23 09:28:03 -0800

    fix image url

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import FirstCalculator from './components/FirstCalculator';
-import SecondCalculator from './components/SecondCalculator';
+import FirstCalculator from "./components/FirstCalculator";
+import SecondCalculator from "./components/SecondCalculator";
 
-import './styles/styles.css';
-
+import "./styles/styles.css";
 
 function App() {
   return (
     <>
       <div className="app-container">
-      <img src="/inside-out-logo.jpeg" />
-      <FirstCalculator />
-    </div>
-    <br />
-    <div className="app-container">
-      <SecondCalculator />
-    </div>
+        <img src="/inside-out-cleaning-calculator-2/inside-out-logo.jpeg" />
+        <FirstCalculator />
+      </div>
+      <br />
+      <div className="app-container">
+        <SecondCalculator />
+      </div>
     </>
   );
 }
